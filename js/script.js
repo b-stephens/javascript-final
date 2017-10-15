873aaa49cdfe2f670136407a52a5af0b769707b9
Bryan Stephens <Bryan@CodedSolutions.io>
2017-10-15 09:39:54 -0700

    added checkboxes, when button is clicked, list is incremented incorrectly

--- js/script.js
+++ js/script.js
@@ -7,59 +7,48 @@ var head = $('header').classList.add('text-center');
 var list = document.createElement('ol');
 var list2 = document.createElement('ol');
 list.setAttribute('style', 'font-size: 30px; color: #4164bf; margin-left: 100px; text-decoration: underline;');
-list2.setAttribute('style', 'font-size: 30px; color: #4164bf; margin-left: 100px; text-decoration: underline;');
+list2.setAttribute('style', 'font-size: 30px; color: #ed2715; margin-left: 100px; text-decoration: underline;');
 var toDo = $('col-1').appendChild(list).textContent ='To Do';
 var done = $('col-2').appendChild(list2).textContent ='Done';
 var button = $('addToList');
 
-var completed = document.createElement('button');
-completed.style.float = 'right';
-completed.innerHTML = 'Completed';
-completed.classList.add('btn-md', 'btn-success');
-
-let toDoList = [];
 
 
 button.addEventListener('click', function() {
     let input = $('input-field').value;
-    let toDoLi = document.createElement('li');
-    toDoLi.innerHTML = input;
-    list.appendChild(toDoLi);
-    toDoLi.appendChild(completed);
-    toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
-    //toDoList.push(input);
-    //console.log(toDoList);
+    if (input.length>0) {
+        var toDoLi;
+        for (var i = 0; i<toDoList.length;i++) {
+            var checkBox = document.createElement('input');
+            checkBox.type = 'checkbox';
+            toDoLi = document.createElement('li');
+            toDoLi.innerHTML = toDoList[i];
+            list.appendChild(toDoLi);
+            toDoLi.appendChild(checkBox);
+        }
+
+
+
+        //toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
+        toDoList.push(input)
+        console.log(toDoList);
+     }
 });
 
-
+let toDoList = [];
 
 
 //appendChild(document.createTextNode(inputValue))
 
 
-// button.addEventListener('click', function(){
-//     if (typeof inputValue === 'string' || inputValue instanceof String) {
-//         console.log(inputValue);
-//     }
-// });
-
-// var body = document.getElementsByName('body'[0]);
-// var $ = function(id) {
-//     return document.getElementById(id);
-// }
-//
-// var head= $('header').classList.add('text-center');
-//
-// var list = document.createElement('ul');
-// var toDoLi = document.createElement('li');
-// var toDo = $('col-1').appendChild(list).textContent = 'To Do';
-// var inputValue = document.forms[0].text.value;
-//
-// var button= $('submit');
-// button.addEventListener('click', function(){
-//
-//     if (typeof inputValue === 'string' || inputValue instanceof String) {
-//         toDoLi.innerHTML = inputValue;
+// button.addEventListener('click', function() {
+//     let input = $('input-field').value;
+//     if (input.length>0) {
+//         let toDoLi = document.createElement('li');
+//         toDoLi.innerHTML = input;
 //         list.appendChild(toDoLi);
+//         toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
+//         toDoList.push(input)
+//         console.log(toDoList);
 //     }
 // });
