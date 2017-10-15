c02833729d1f8f45fdf7b37d493e935bb1c4f516
Bryan Stephens <Bryan@CodedSolutions.io>
2017-10-15 15:26:19 -0700

    text branch, incrementing correctly, but checkboxes not showing up

--- js/script.js
+++ js/script.js
@@ -6,49 +6,42 @@ var $ = function(id) {
 var head = $('header').classList.add('text-center');
 var list = document.createElement('ol');
 var list2 = document.createElement('ol');
-list.setAttribute('style', 'font-size: 30px; color: #4164bf; margin-left: 100px; text-decoration: underline;');
-list2.setAttribute('style', 'font-size: 30px; color: #ed2715; margin-left: 100px; text-decoration: underline;');
-var toDo = $('col-1').appendChild(list).textContent ='To Do';
-var done = $('col-2').appendChild(list2).textContent ='Done';
+list.setAttribute('style', 'font-size: 18px; color: #4164bf; margin-left: 100px; text-decoration: underline; font-family: Coming Soon, cursive;');
+list2.setAttribute('style', 'font-size: 18px; color: #ed2715; margin-left: 100px; text-decoration: underline; font-family: Coming Soon, cursive;');
+var toDo = $('col-1').appendChild(list);
+var done = $('col-2').appendChild(list2);
+var input = $('inputField');
 var button = $('addToList');
+var toDoList = new Array();
 
 
+var addToList = function(inputField) {
+    addToArray(inputField);
+    refreshList();
+};
 
-button.addEventListener('click', function() {
-    let input = $('input-field').value;
-    if (input.length>0) {
-        var toDoLi;
-        for (var i = 0; i<toDoList.length;i++) {
-            var checkBox = document.createElement('input');
-            checkBox.type = 'checkbox';
-            toDoLi = document.createElement('li');
-            toDoLi.innerHTML = toDoList[i];
-            list.appendChild(toDoLi);
-            toDoLi.appendChild(checkBox);
-        }
+var addToArray = function(text) {
+    toDoList.push(input.value);
+};
 
+var refreshList = function() {
+    list.innerHTML = '';
+    list2.innerHTML = '';
+    var listItem;
+    for (var i = 0; i<toDoList.length; i++) {
+        var checkBox = document.createElement ('checkbox');
+        checkBox.type = 'checkbox';
+        listItem = document.createElement('li');
+        listItem.innerHTML = toDoList[i];
+        list.appendChild(listItem);
+        listItem.appendChild(checkBox);
+    };
+};
 
-
-        //toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
-        toDoList.push(input)
-        console.log(toDoList);
-     }
+button.addEventListener('click', function(e) {
+    var inputField = input.value;
+    addToList(inputField);
+    refreshList();
 });
 
-let toDoList = [];
-
-
-//appendChild(document.createTextNode(inputValue))
-
-
-// button.addEventListener('click', function() {
-//     let input = $('input-field').value;
-//     if (input.length>0) {
-//         let toDoLi = document.createElement('li');
-//         toDoLi.innerHTML = input;
-//         list.appendChild(toDoLi);
-//         toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
-//         toDoList.push(input)
-//         console.log(toDoList);
-//     }
-// });
+console.log(addToArray());
