971f195ae37bcc9350f5b6e5bc9160b9bf6fadc5
Bryan Stephens <Bryan@CodedSolutions.io>
2017-10-15 18:49:12 -0700

    trying to figure out enter event, first item submitted doesn't append to list

--- js/script.js
+++ js/script.js
@@ -6,30 +6,28 @@ var $ = function(id) {
 var head = $('header').classList.add('text-center');
 var list = document.createElement('ol');
 var list2 = document.createElement('ol');
-list.setAttribute('style', 'font-size: 30px; color: #4164bf; margin-left: 100px; text-decoration: underline;');
-list2.setAttribute('style', 'font-size: 30px; color: #ed2715; margin-left: 100px; text-decoration: underline;');
-var toDo = $('col-1').appendChild(list).textContent ='To Do';
-var done = $('col-2').appendChild(list2).textContent ='Done';
+list.setAttribute('style', 'font-size: 18px; color: #4164bf; margin-left: 100px; text-decoration: underline;');
+list2.setAttribute('style', 'font-size: 18px; color: #ed2715; margin-left: 100px; text-decoration: underline;');
+var toDo = $('col-1').appendChild(list);
+var done = $('col-2').appendChild(list2);
 var button = $('addToList');
 
 
-
-button.addEventListener('click', function() {
+button.addEventListener('click', function clickButton() {
     let input = $('input-field').value;
+    list.innerHTML = '';
+    list2.innerHTML = '';
     if (input.length>0) {
         var toDoLi;
         for (var i = 0; i<toDoList.length;i++) {
             var checkBox = document.createElement('input');
             checkBox.type = 'checkbox';
+            checkBox.setAttribute('style', 'float: right; margin-right: 80px;')
             toDoLi = document.createElement('li');
             toDoLi.innerHTML = toDoList[i];
             list.appendChild(toDoLi);
             toDoLi.appendChild(checkBox);
         }
-
-
-
-        //toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
         toDoList.push(input)
         console.log(toDoList);
      }
@@ -37,18 +35,3 @@ button.addEventListener('click', function() {
 
 let toDoList = [];
 
-
-//appendChild(document.createTextNode(inputValue))
-
-
-// button.addEventListener('click', function() {
-//     let input = $('input-field').value;
-//     if (input.length>0) {
-//         let toDoLi = document.createElement('li');
-//         toDoLi.innerHTML = input;
-//         list.appendChild(toDoLi);
-//         toDoLi.setAttribute('style', 'color: #000; font-size: 18px; text-decoration: none; margin-left: -30px; font-family: Coming Soon, cursive; font-size: 25px;');
-//         toDoList.push(input)
-//         console.log(toDoList);
-//     }
-// });
