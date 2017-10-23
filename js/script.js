a2c92a0b319cf61926d055a5af375278f39b0a7b
Bryan Stephens <Bryan@CodedSolutions.io>
2017-10-22 19:43:58 -0700

    done btn only appends first to do item to done list and does line-thru on last item.  Need to figure out edit btn too. And clear btn

--- js/script.js
+++ js/script.js
@@ -1,42 +1,96 @@
-var body = document.getElementsByName('body'[0]);
 var $ = function(id) {
     return document.getElementById(id);
-}
+};
 
-var head = $('header').classList.add('text-center');
-var list = document.createElement('ol');
-var list2 = document.createElement('ol');
-list.setAttribute('style', 'font-size: 18px; color: #4164bf; margin-left: 100px; font-family: Coming Soon, cursive');
-list2.setAttribute('style', 'font-size: 18px; color: #ed2715; margin-left: 100px; font-family: Coming Soon, cursive');
-var toDo = $('col-1').appendChild(list);
-var done = $('col-2').appendChild(list2);
-var button = $('addToList');
+var toDoList = [];
+var doneList = [];
 
-//creates value for text box as soon as content is entered
-//  var input = $('input-field');
-//  var inputText = input.addEventListener('keyup', function(e) {
-//      $('input-field').value;
-//  })
-
-button.addEventListener('click', function clickButton() {
-    let input = $('input-field').value;
-    list.innerHTML = '';
-    list2.innerHTML = '';
-    if (input.length>0) {
-        var toDoLi;
-        for (var i = 0; i<toDoList.length;i++) {
-            var checkBox = document.createElement('input');
-            checkBox.type = 'checkbox';
-            checkBox.setAttribute('style', 'float: right; margin-right: 80px;')
-            toDoLi = document.createElement('li');
-            toDoLi.innerHTML = toDoList[i];
-            list.appendChild(toDoLi);
-            toDoLi.appendChild(checkBox);
-        }
-        toDoList.push(input)
-        console.log(toDoList);
-     }
+var addToList = $('add-button');
+addToList.addEventListener('click', function() {
+    event.preventDefault();
+    var textField = $('input-field').value;
+    if (textField.length>0) {
+        toDoList.push(textField);
+        makeList('to-do');
+        document.forms[0].reset();
+        console.log('to-do: ' + toDoList);
+    };
 });
 
-let toDoList = [];
+function makeList(list_type) {
+    if(list_type == 'to-do') {
+        var col = $('to-do-list');
+        var itemsArray = toDoList;
+        var change = true;
+    } else {
+        var col = $('done-list');
+        var itemsArray = doneList;
+    }
+    col.innerHTML = '';
+
+    for(var i = 0; i<itemsArray.length; i++) {
+        if (change) {
+            var listItem = document.createElement('li');
+            var editBtn = document.createElement('button');
+            var doneBtn = document.createElement('button');
+
+            listItem.innerHTML = itemsArray[i];
+
+            listItem.appendChild(editBtn);
+            listItem.appendChild(doneBtn);
+
+            editBtn.innerText = ('Edit');
+            editBtn.setAttribute('type', 'button');
+            editBtn.classList.add('btn', 'btn-sm', 'btn-default');
+            editBtn.addEventListener('click', function() {
+                let newValue = prompt(itemsArray[i])
+                newValue = itemsArray[i];
+            });
+
+            doneBtn.innerText = ('Done');
+            doneBtn.setAttribute('type', 'button');
+            doneBtn.classList.add('btn', 'btn-sm', 'btn-danger');
+
+            doneBtn.addEventListener('click', delayDoneList, moveToDone);
+            doneBtn.addEventListener('click', function() {
+                listItem.setAttribute('style', 'text-decoration: line-through');
+            });
+            col.appendChild(listItem);
+
+        } else {
+
+            var doneItems = document.createElement('li');
+            doneItems.innerHTML = itemsArray[i];
+            col.appendChild(doneItems);
+            var clearDoneList = $('clearBtn');
+            clearDoneList.addEventListener('click', function() {
+                col.innerHTML = '';
+                doneList = [''];
+            });
+        };
+    };
+};
+
+function moveToDone(i) {
+    doneList.push(toDoList.splice(i, 1));
+    makeList('to-do');
+    makeList('done');
+    console.log('done: ' + doneList);
+};
+
+var timer
+function delayDoneList() {
+    timer = setTimeout(moveToDone, 2000);
+};
+
+
+
+
+
+
+
+
+
+
+
 
